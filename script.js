000df3bf991abf34c8f28c2f6a17718a5ff0385f
Stefano <stefanopinna92@hotmail.it>
2023-12-06 20:45:15 +0000

    Working on add to library function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
-const bookCard = document.querySelector('.card');
-const bookTitle = document.querySelector('.title-sec');
+const mainContainer = document.querySelector('.container');
+
 const bookAuthor = document.querySelector('.author-sec'); 
 const bookPages = document.querySelector('.pages-sec');
 const bookInfo = document.querySelector('.info-sec');
@@ -19,23 +19,44 @@ Book.prototype.info = function() {
 }
 
 let bookOne = new Book('Walden', 'Henry David Thoureau', 198, 'loved the nature and freedom of the character');
-let bookTwo = new Book('Family Happiness', 'Leo Tolstoy', 220, 'taught me how important being around people is')
+let bookTwo = new Book('Family Happiness', 'Leo Tolstoy', 220, 'taught me how important being around people is');
+let bookThree = new Book('White Nights', 'Fyodor Dostoevsky', 276, 'read long time ago. About being a dreamful young'); 
 
-/*
-function addBookToLibrary(book) {
-       myLibrary.push(book);
-       return myLibrary.forEach((book) => ); /* return function that create a card with all details of the book. Should this function have some common details and therefore you can create an example before or should it create p and divs from scratch all the time 
-}; */
+
 
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    return myLibrary.forEach((book) => 
-       bookTitle.textContent = book.title,
-       bookAuthor.textContent = book.author,
-       bookPages.textContent = book.pages,
-       bookInfo.textContent = book.info()); 
 
+    
+    return myLibrary.forEach( book => {
+       
+       let bookCard = document.createElement('div');
+       let bookTitle = document.createElement('h3');
+       let bookAuthor = document.createElement('h4');
+       let bookPages = document.createElement('p'); 
+       let bookInfo = document.createElement('p');
+       bookCard.classList.add('card');
+       mainContainer.appendChild(bookCard);
+       bookCard.appendChild(bookTitle);
+       bookCard.appendChild(bookAuthor);
+       bookCard.appendChild(bookPages);
+
+       bookCard.appendChild(bookInfo); 
+       bookTitle.textContent = book.title;
+       bookAuthor.textContent = book.author; 
+       bookPages.textContent = book.pages;
+       bookInfo.textContent = book.info();
+
+
+   
+    }),
+  myLibrary.pop();
 };
 
-addBookToLibrary(bookOne); 
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
+addBookToLibrary(bookThree); 
+
+
+
